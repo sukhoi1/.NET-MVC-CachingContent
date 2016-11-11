dbc6d3b1dcc34148c70fc004caefe813862ccf04
Anton Lygin <lygin@live.com>
2016-11-11 23:57:49 +0300

    v0.005 - varyByHeader and varyByParam;

---
+++ MvcCachingContent/Controllers/VaryByHeaderController.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+using System.Web.UI;
+using MvcCachingContent.Infrastructure;
+
+namespace MvcCachingContent.Controllers
+{
+    public class VaryByHeaderController : Controller
+    {
+        [OutputCache(Duration = 30, VaryByHeader = "user-agent",
+            Location = OutputCacheLocation.Any)]
+        public ActionResult Index()
+        {
+            int counterValue = AppStateHelper.IncrementAndGet(AppStateKeys.IndexCounter);
+            Debug.WriteLine($"IndexCounter: {counterValue}");
+            return View("~/Views/Shared/One.cshtml", (object)counterValue);
+        }
+    }
+}
