e93962ef24eb7fbe99c4b83a5ac11db6c8528991
Anton Lygin <lygin@live.com>
2016-11-14 00:12:30 +0300

    v0.007 - Cache profiles (config) example;

---
+++ MvcCachingContent/Controllers/CacheProfilesController.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+using MvcCachingContent.Infrastructure;
+
+namespace MvcCachingContent.Controllers
+{
+    public class CacheProfilesController : Controller
+    {
+        [OutputCache(CacheProfile = "cp1")]
+        public ActionResult Index()
+        {
+            int counterValue = AppStateHelper.IncrementAndGet(AppStateKeys.IndexCounter);
+            Debug.WriteLine($"IndexCounter: {counterValue}");
+            return View("~/Views/Shared/One.cshtml", (object)counterValue);
+        }
+    }
+}
