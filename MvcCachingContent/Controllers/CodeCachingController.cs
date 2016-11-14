ed0a8498f8dc6bc79617d3ff037f5bf76c9e5c94
Anton Lyhin <Anton_Lyhin@epam.com>
2016-11-14 14:23:33 +0300

    v0.013 - Configure caching from code example;

---
+++ MvcCachingContent/Controllers/CodeCachingController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+using MvcCachingContent.Infrastructure;
+
+namespace MvcCachingContent.Controllers
+{
+    public class CodeCachingController : Controller
+    {
+        CachingConfigurationLogic cacheSettingsHelper = new CachingConfigurationLogic();
+
+        public ActionResult Index()
+        {
+            cacheSettingsHelper.DetectConfiguration(Request.RawUrl, Response.Cache);
+
+            int counterValue = AppStateHelper.IncrementAndGet(AppStateKeys.IndexCounter);
+            Debug.WriteLine($"IndexCounter: {counterValue}");
+            return View("~/Views/CachingChildAction/Index.cshtml", (object)counterValue);
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult GetTime()
+        {
+            return PartialView("~/Views/CachingChildAction/GetTime.cshtml", (object)DateTime.Now.ToLongTimeString());
+        }
+    }
+}
---
+++ MvcCachingContent/Infrastructure/CachingConfigurationLogic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web;
+
+namespace MvcCachingContent.Infrastructure
+{
+    public class CachingConfigurationLogic
+    {
+        /// <summary>
+        /// It is better to detect controller and action rather than plain url (such as "/Home/Index", "/Home", "/" since routes are equal by default).
+        /// </summary>
+        public void DetectConfiguration(string url, HttpCachePolicyBase cachePolicy)
+        {
+            if (url == "/CodeCaching/Index")
+            {
+                cachePolicy.SetNoServerCaching();
+                cachePolicy.SetCacheability(HttpCacheability.NoCache);
+            }
+            else
+            {
+                cachePolicy.SetExpires(DateTime.Now.AddSeconds(30));
+                cachePolicy.SetCacheability(HttpCacheability.Public);
+            }
+        }
+    }
+}
