35895289bca8824056600faf6a89ba97286707a7
Anton Lyhin <Anton_Lyhin@epam.com>
2016-11-14 13:31:28 +0300

    v0.012 - Disable caching from web.config and without code recompilation;

---
+++ MvcCachingContent/Controllers/CacheProfilesDisableController.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+using MvcCachingContent.Infrastructure;
+
+namespace MvcCachingContent.Controllers
+{
+    public class CacheProfilesDisableController : Controller
+    {
+        [OutputCache(CacheProfile = "cp1-disable")]
+        public ActionResult Index()
+        {
+            int counterValue = AppStateHelper.IncrementAndGet(AppStateKeys.IndexCounter);
+            Debug.WriteLine($"IndexCounter: {counterValue}");
+            return View((object)counterValue);
+        }
+    }
+}
