59a0192be5c23162a509826fa39194c0f855290c
Anton Lyhin <Anton_Lyhin@epam.com>
2016-11-14 12:31:17 +0300

    v0.010 - Donut hole caching example;

---
+++ MvcCachingContent/Controllers/DonutHoleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+using MvcCachingContent.Infrastructure;
+
+namespace MvcCachingContent.Controllers
+{
+    public class DonutHoleController : Controller
+    {
+        public ActionResult Index()
+        {
+            int counterValue = AppStateHelper.IncrementAndGet(AppStateKeys.IndexCounter);
+            Debug.WriteLine($"IndexCounter: {counterValue}");
+            return View("~/Views/CachingChildAction/Index.cshtml", (object)counterValue);
+        }
+
+        [ChildActionOnly]
+        [OutputCache(Duration = 20)]
+        public PartialViewResult GetTime()
+        {
+            return PartialView("~/Views/CachingChildAction/GetTime.cshtml", (object)DateTime.Now.ToShortTimeString());
+        }
+    }
+}
