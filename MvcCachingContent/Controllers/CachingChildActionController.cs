ae943e837b809c528003fdcf307e88f1ecd4d66d
Anton Lyhin <Anton_Lyhin@epam.com>
2016-11-14 12:18:40 +0300

    v0.008 - Cache child action;

---
+++ MvcCachingContent/Controllers/CachingChildActionController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Web.Mvc;
+using MvcCachingContent.Infrastructure;
+
+namespace MvcCachingContent.Controllers
+{
+    public class CachingChildActionController : Controller
+    {
+        [OutputCache(CacheProfile = "cp1")]
+        public ActionResult Index()
+        {
+            int counterValue = AppStateHelper.IncrementAndGet(AppStateKeys.IndexCounter);
+            Debug.WriteLine($"IndexCounter: {counterValue}");
+            return View((object)counterValue);
+        }
+
+        [ChildActionOnly]
+        [OutputCache(Duration = 60)]
+        public PartialViewResult GetTime()
+        {
+            return PartialView((object)DateTime.Now.ToShortTimeString());
+        }
+    }
+}
